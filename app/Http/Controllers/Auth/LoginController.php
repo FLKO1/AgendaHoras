06ac87670959d28d031339477cce3d83236f3300
FLKO1 <83712998+FLKO1@users.noreply.github.com>
2023-07-11 01:46:03 -0400

    Login Modificacion
    Se agrego los metodos de register, login, logout(Nesesario probar)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,18 +6,59 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+
+use Illuminate\Http\Request;
+use App\Models\User;
+Use Illuminate\Support\Facades\Hash;
+Use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+
+    //
+    public function register(Request $request){
+        
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+        Auth::login($user);
+
+        return redirect(route('privada'));
+    }
+
+
+    public function login(Request $request){
+        
+        
+        $credentials = [
+            "email"=> $request->email,
+            "password"=>$request->password,
+        ];
+        $remember = ($request->has('remember')? true : false);
+        
+        if(Auth::attempt($credentials,$remember)){
+            $request->session()->regenerate();
+            return redirect()->intended(route('privada'));
+
+        }else{
+            return redirect('login');
+        }
+    }
+
+    public function logout(Request $request){
+
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect(route('login'));
+    }
+
+
 
     use AuthenticatesUsers;
 
@@ -26,7 +67,14 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+   
+     //protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function redirectTo()
+{
+    return '/index';
+}
+
 
     /**
      * Create a new controller instance.
